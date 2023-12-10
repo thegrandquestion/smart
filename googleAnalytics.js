6b47db6f7de9da0879ab13d62c12dad60600762d
Smart Revise <johncairofahim@gmail.com>
2023-12-10 14:02:59 +0000

    12/12/23 14:02

--- googleAnalytics.js
+++ googleAnalytics.js
@@ -1,20 +1,20 @@
 // googleAnalytics.js
 key = ""
-const jaj = ["RA|I|I|I","Ijaz2036","MZ|I|I|I","IH|I|I|I","SH|I|I|I","IQ|I|I|I","TahseensBalls"]
+const allowed = ["RA|I|I|I","Ijaz2036","MZ|I|I|I","IH|I|I|I","SH|I|I|I","IQ|I|I|I","TahseensBalls"]
 function verify(){
     const aja = prompt("Revision Time! Say YAY")
-    for (let i = 0; i < jaj.length; i++) {;
+    for (let i = 0; i < allowed.length; i++) {;
         if (aja == "YAY") {
             window.location.href = "https://smartrevise.online/student/home/Index";
             break;
         }
-        if (aja === jaj[i]) {
+        if (aja === allowed[i]) {
             return aja
         } else if (aja === "MASTERKEY"){
             document.getElementById("hiddenLinks").style.visibility = "visible";
             return aja
         } else {
-            if (i === jaj.length - 1)
+            if (i === allowed.length - 1)
             window.location.href = "https://smartrevise.online/student/home/Index";
         }
     }
@@ -39,5 +39,28 @@ function rot13(str) {
       }
     }
   }
+  return strArray.join("");
+}
+function rotMinus13(str) {
+  let strArray = str.split("");
+
+  for (let i = 0; i < strArray.length; i++) {
+    strArray[i] = strArray[i].toUpperCase();
+  }
+
+  for (let i = 0; i < strArray.length; i++) {
+    for (let a = 0; a < alphabet.length; a++) {
+      if (strArray[i] == alphabet[a]) {
+        let abc = a - 13; // Change the rotation value to -13
+        if (abc < 0) {
+          let rem = (abc + 36) % 36; // Adjust for negative values
+          abc = rem;
+        }
+        strArray[i] = alphabet[abc];
+        break;
+      }
+    }
+  }
+
   return strArray.join("");
 }
