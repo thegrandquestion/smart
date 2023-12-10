1ee7500560f8e101ae88819753f9d38e6cf28104
Smart Revise <johncairofahim@gmail.com>
2023-12-10 13:06:10 +0000

    Update googleAnalytics.js

--- googleAnalytics.js
+++ googleAnalytics.js
@@ -18,16 +18,19 @@ function verify(){
         }
     }
 }
-const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
 function rot13(str) {
   let strArray = str.split("")
+  for (let i = 0; i < strArray.length; i++) {
+    strArray[i] = strArray[i].toUpperCase();
+  }
   console.log(strArray.join(""))
   for (let i = 0; i < strArray.length;i++){
     for (let a = 0;a < alphabet.length;a++){
       if (strArray[i] == alphabet[a] ){
         let abc = a + 13
-        if (abc > 25){
-          let rem = abc % 26
+        if (abc > 35){
+          let rem = abc % 36
           abc = rem
         }
         strArray[i] = alphabet[abc]
